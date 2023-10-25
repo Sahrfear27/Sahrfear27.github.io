3246efda36fdbc1acbcbbd7702faa2ed2313886e
sahrfearmacarthy <macarthysahralves>
2023-10-25 08:07:20 -0500

    commit changes

--- CIS405/WK3/chartWindow.js
+++ CIS405/WK3/chartWindow.js
@@ -5,6 +5,7 @@ function setActiveUser(user)
  {
     activeUser = user;
     let activeUserElement = document.getElementById('active-user');
+    activeUserElement.style.color = "black"
     activeUserElement.innerHTML = user + " is typing...";
 }
 
@@ -19,19 +20,25 @@ function sendMessage()
     {
         p.style.color="white";
         p.style.background="blue";
-        p.style.border="1px solid";
-          p.style.borderRadius="10px";
+        p.style.borderRadius="10px";
         p.style.textAlign="left";
         p.style.padding="5px";
+        p.style.width = "max-content"
+        p.style.margin="10px";
+        p.style.marginTop="20px";
 
     } else if (activeUser === "Sahrfear") 
     {
         p.style.color="white";
-        p.style.background="grey";
-        p.style.border="1px solid";
+        p.style.background="#343A3F";
         p.style.borderRadius="10px";
         p.style.textAlign="right";
         p.style.padding="5px";
+        p.style.float = "right";
+        p.style.width = "max-content"
+        p.style.margin="10px";
+        p.style.marginBottom="20px";
+       
     }
 
     typingArea.appendChild(p);
