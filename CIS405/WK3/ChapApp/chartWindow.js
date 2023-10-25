b1a5b227d999c3a69cb10a2274f7d9e974286fb8
sahrfearmacarthy <macarthysahralves>
2023-10-25 16:45:10 -0500

    commit changes

--- CIS405/WK3/ChapApp/chartWindow.js
+++ CIS405/WK3/ChapApp/chartWindow.js
@@ -17,34 +17,46 @@ function setActiveUser(user)
 function sendMessage() 
 {
     let typingArea = document.getElementById('mainText');
-    let typingMessage = document.getElementById('type-message');
+    let typingMessage = document.getElementById('type-message').value;
     let p = document.createElement('p');
-    p.innerHTML = activeUser + ': ' + typingMessage.value;
-    
-
-    if (activeUser === "Sagar")
+    let br = document.createElement('br');
+    if((typingMessage) && (activeUser === "Sagar"))
     {
+        p.innerHTML = activeUser + ': ' + typingMessage;
         p.style.color="white";
         p.style.background="blue";
         p.style.borderRadius="10px";
         p.style.textAlign="left";
         p.style.padding="5px";
-        p.style.width = "max-content"
-        // p.style.textShadow = "1px 2px 7px rgba(0,0,0,0.6)";
-
-     
-
-    } else if (activeUser === "Sahrfear") 
+        p.style.width = "max-content";
+        // p.style.display = "inline-block";
+        p.style.float = "left";
+    }
+    else if((typingMessage) && (activeUser === "Sahrfear"))
     {
+        p.innerHTML = activeUser + ': ' + typingMessage ;
         p.style.color="white";
         p.style.background="#343A3F";
         p.style.borderRadius="10px";
         p.style.textAlign="right";
         p.style.padding="5px";
-        // p.style.textShadow = "1px 2px 7px rgba(0,0,0,0.6)";
-       
+        // p.style.display = "inline-block";
+        p.style.float = "right";
     }
-
+    else{
+        p.innerHTML = "";
+        p.style.color = "red"
+    }
     typingArea.appendChild(p);
+    // Append br to the typing area
+    // typingArea.appendChild(br);
     typingMessage.value = '';
+}
+
+
+function clearChat(){
+    let clearChat = document.getElementById('mainText');
+    clearChat.style.display = "none";
+    clearChat = "";
+
 }
