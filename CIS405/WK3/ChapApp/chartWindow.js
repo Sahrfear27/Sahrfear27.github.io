035e4433ea57b2402a7c6306cf985c87b8b753a8
sahrfearmacarthy <macarthysahralves>
2023-10-25 14:02:52 -0500

    commit changes

--- CIS405/WK3/ChapApp/chartWindow.js
+++ CIS405/WK3/ChapApp/chartWindow.js
@@ -5,7 +5,12 @@ function setActiveUser(user)
  {
     activeUser = user;
     let activeUserElement = document.getElementById('active-user');
-    activeUserElement.style.color = "black"
+    activeUserElement.style.color = "black";
+    activeUserElement.style.textShadow = "1px 12px 9px rgba(0,0,0,0.6)";
+    activeUserElement.style.fontWeight = "bold";
+    activeUserElement.style.letterSpacing = "3px";
+
+
     activeUserElement.innerHTML = user + " is typing...";
 }
 
@@ -15,6 +20,7 @@ function sendMessage()
     let typingMessage = document.getElementById('type-message');
     let p = document.createElement('p');
     p.innerHTML = activeUser + ': ' + typingMessage.value;
+    
 
     if (activeUser === "Sagar")
     {
@@ -24,6 +30,8 @@ function sendMessage()
         p.style.textAlign="left";
         p.style.padding="5px";
         p.style.width = "max-content"
+        // p.style.textShadow = "1px 2px 7px rgba(0,0,0,0.6)";
+
      
 
     } else if (activeUser === "Sahrfear") 
@@ -33,7 +41,7 @@ function sendMessage()
         p.style.borderRadius="10px";
         p.style.textAlign="right";
         p.style.padding="5px";
-      
+        // p.style.textShadow = "1px 2px 7px rgba(0,0,0,0.6)";
        
     }
 
--- CIS405/WK3/ChapApp/login.js
+++ CIS405/WK3/ChapApp/login.js
@@ -6,3 +6,64 @@ params.forEach((key,value)=>{
     paragraph.append(`${key}= ${value}`);
     paragraph.append(document.createElement('br'));
 })
+
+
+localStorage.setItem('first-name', fNameValue);
+localStorage.setItem('last-name', lNameValue);
+localStorage.setItem('valid-password', setPassword);
+
+// Get the first name value
+
+let firstName = localStorage.setItem('first-Name', )
+
+
+let  = localStorage.getItem('');
+localStorage.setItem('signInDetails',JSON.stringify(accessToLogInPage));
+
+
+
+// Validating the inputData
+const validateInputs = () => {
+const firstNameValue = first-Name.value.trim();
+const lastNameValue = last-Name.value.trim();
+const password = newPass.value.trim();
+  
+}
+
+function signIn(){
+    let submitButton = document.getElementById('submit');
+
+}
+    // VAalidation for first name
+//     if (firstNameValue === "") {
+//       setError(firstName, "Firstname is required");
+//     } else {
+//       setSuccess(firstName);
+//     }
+  
+//     // Validation for last name
+//     if (lastNameValue === "") {
+//       setError(lastName, "Lastname is required");
+//     } else {
+//       setSuccess(lastName);
+//     }
+  
+//     // Validation for newPassword
+//     if (newPassValue === "") {
+//       setError(newPass, "Password is required");
+//     } else if (newPassValue.length < 6) {
+//       setError(newPass, "Password must be atleast 6 character");
+//     } else {
+//       setSuccess(newPass);
+//     }
+  
+//     // Validation for confirm password
+//     if (confirmPasswordValue === "") {
+//       setError(confirmPassword, "Please confirm your password");
+//     } else if (confirmPasswordValue !== newPassValue) {
+//       setError(confirmPassword, "Password doesn't match");
+//     } else {
+//       setSuccess(confirmPassword);
+//     }
+//   };
+  
