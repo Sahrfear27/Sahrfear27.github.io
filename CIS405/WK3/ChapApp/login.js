ace20aca4c248b32d208e1c831a39d854d5cdd0a
sahrfearmacarthy <macarthysahralves>
2023-10-25 20:09:36 -0500

    commit changes

--- CIS405/WK3/ChapApp/login.js
+++ CIS405/WK3/ChapApp/login.js
@@ -1,69 +1,68 @@
-const paragraph = document.querySelector('#para');
 
-const params = new URLSearchParams(window.location.search);
 
-params.forEach((key,value)=>{
-    paragraph.append(`${key}= ${value}`);
-    paragraph.append(document.createElement('br'));
+const form = document.getElementById('form');
+const firstName = document.getElementById('loginFirstName');
+const secondName = document.getElementById('loginLastName');
+const password = document.getElementById('loginPassword');
+const storedFirstName = localStorage.getItem('first-name')
+const storedSecondName = localStorage.getItem('last-name');
+const storedPassword = localStorage.getItem('valid-password');
+
+form.addEventListener('submit', (e) =>{
+    validateInputs();
+    if(isFormValid() == true){
+        form.submit();
+    }else{
+        e.preventDefault();  
+    }
 })
 
+function isFormValid() {
+    const inputContainers = form.querySelectorAll('.form_group');
+    let result = true;
+    inputContainers.forEach((container)=>{
+        if(container.classList.contains('error')){
+            result =false;
+        }
+    })
+    return result;
+}
 
-localStorage.setItem('first-name', fNameValue);
-localStorage.setItem('last-name', lNameValue);
-localStorage.setItem('valid-password', setPassword);
+const setError =(element,message) =>{
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+};
+const setSuccess = (element) =>{
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    errorDisplay.innerText =' ';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
 
-// Get the first name value
-
-let firstName = localStorage.setItem('first-Name', )
-
-
-let  = localStorage.getItem('');
-localStorage.setItem('signInDetails',JSON.stringify(accessToLogInPage));
-
-
-
-// Validating the inputData
 const validateInputs = () => {
-const firstNameValue = first-Name.value.trim();
-const lastNameValue = last-Name.value.trim();
-const password = newPass.value.trim();
-  
-}
+    const firstNameValue = firstName.value.trim();
+    const secondNameValue = secondName.value.trim();
+    const passwordValue = password.value.trim();
 
-function signIn(){
-    let submitButton = document.getElementById('submit');
+    if(firstNameValue !== storedFirstName){
+        setError(firstName,'invalid firstname');
+    }else{
+        setSuccess(firstName);
+    }
 
-}
-    // VAalidation for first name
-//     if (firstNameValue === "") {
-//       setError(firstName, "Firstname is required");
-//     } else {
-//       setSuccess(firstName);
-//     }
-  
-//     // Validation for last name
-//     if (lastNameValue === "") {
-//       setError(lastName, "Lastname is required");
-//     } else {
-//       setSuccess(lastName);
-//     }
-  
-//     // Validation for newPassword
-//     if (newPassValue === "") {
-//       setError(newPass, "Password is required");
-//     } else if (newPassValue.length < 6) {
-//       setError(newPass, "Password must be atleast 6 character");
-//     } else {
-//       setSuccess(newPass);
-//     }
-  
-//     // Validation for confirm password
-//     if (confirmPasswordValue === "") {
-//       setError(confirmPassword, "Please confirm your password");
-//     } else if (confirmPasswordValue !== newPassValue) {
-//       setError(confirmPassword, "Password doesn't match");
-//     } else {
-//       setSuccess(confirmPassword);
-//     }
-//   };
-  
+    if(secondNameValue!==storedSecondName){
+        setError(secondName,'invalid secondname');   
+    }else{
+        setSuccess(secondName);
+    }
+
+    if(passwordValue !== storedPassword){
+        setError(password,"Invalid password");
+    }else{
+        setSuccess(password);
+    }
+};
