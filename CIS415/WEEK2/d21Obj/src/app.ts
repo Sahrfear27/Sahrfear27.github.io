895fb4cf44767a4831189b60beef08463a80a211
sahrfearmacarthy <macarthysahralves>
2023-11-07 21:58:36 -0600

    commit changes

--- CIS415/WEEK2/d21Obj/src/app.ts
+++ CIS415/WEEK2/d21Obj/src/app.ts
@@ -1,22 +1,22 @@
 
-// //use this file for any code you want to try out
+//use this file for any code you want to try out
 
-// // d21Arrays
-// // Write code according to the Mocha tests in quizTests.ts in today’s assignment code from the
-// // course repository. First, review the following setup that is in the Mocha test code. Find three objects,
-// // student1, student2, student3 with the following properties: 
+// d21Arrays
+// Write code according to the Mocha tests in quizTests.ts in today’s assignment code from the
+// course repository. First, review the following setup that is in the Mocha test code. Find three objects,
+// student1, student2, student3 with the following properties: 
 
 
-// export function StudentQuiz(quizAnswers: number[], correctAnswers : number[]){
+export function StudentQuiz(quizAnswers: number[], correctAnswers : number[]){
     
-// }
-// export  function gradeQuizLabeled(){
+}
+export  function gradeQuizLabeled(){
     
-// }
+}
 
-// export function gradeStudent(){
+export function gradeStudent(){
 
-// }
-// export function gradeQuiz(){
+}
+export function gradeQuiz(){
 
-// }
+}
--- CIS415/WEEK2/d21Obj/src/book.ts
+++ CIS415/WEEK2/d21Obj/src/book.ts
@@ -1,146 +1,128 @@
-// /*
-// o	createBook, which will take title, author, and libraryID as inputs.  It will create a new book object and add it to the library, 
-// which will be represented as a global array named libraryBooks.  createBook should return the newly created book.
-// o	findTitles, which will find all the book titles in libraryBooks and return them in an alphabetically ordered array.
-// o	findAuthors, which will find all the authors in libraryBooks and return them in an alphabetically ordered array.
-// o	findIDs, which will find all the libraryIDs in libraryBooks and return them in an alphabetically ordered array.
-// */
+/*
+o	createBook, which will take title, author, and libraryID as inputs.  It will create a new book object and add it to the library, 
+which will be represented as a global array named libraryBooks.  createBook should return the newly created book.
+o	findTitles, which will find all the book titles in libraryBooks and return them in an alphabetically ordered array.
+o	findAuthors, which will find all the authors in libraryBooks and return them in an alphabetically ordered array.
+o	findIDs, which will find all the libraryIDs in libraryBooks and return them in an alphabetically ordered array.
+*/
 
-// export interface Book {
-//     title: string,
-//     author: string,
-//     libraryID: number
-// }
+export type Book = {
+    title: string,
+    author: string,
+    libraryID: number
+}
 
-// export let library: Book[] = [
-//     { title: "The Road Ahead", author: "Bill Gates", libraryID: 1254 },
-//     { title: "Walter Isaacson", author: "Steve Jobs", libraryID: 4264 },
-//     { title: "Mockingjay: The Final Book of The Hunger Games", author: "Suzanne Collins", libraryID: 3245 }
-// ];
+export let library: Book[] = [
+    { title: "The Road Ahead", author: "Bill Gates", libraryID: 1254 },
+    { title: "Walter Isaacson", author: "Steve Jobs", libraryID: 4264 },
+    { title: "Mockingjay: The Final Book of The Hunger Games", author: "Suzanne Collins", libraryID: 3245 }
+];
 
-// /**
-//  * Event handler to display library titles sorted alphabetically
-//  * @returns {undefined}
-//  */
-// export function showTitles(): void {
-//     /* this function is complete, no need to modify.  Inspect it as an example for the other functions. */
-//     /* put all titles into an array, then sort, then join with newline and insert in textarea innerHTML */
+/**
+ * Event handler to display library titles sorted alphabetically
+ * @returns {undefined}
+ */
+export function showTitles(): void {
+    /* this function is complete, no need to modify.  Inspect it as an example for the other functions. */
+    /* put all titles into an array, then sort, then join with newline and insert in textarea innerHTML */
 
-//     const titles = findTitles();
+    const titles = findTitles();
 
-//     /*need to sort and then join the titles still (e.g., someArray.join("\n")  */
-//     titles.sort();
-//     const titleString = titles.join("\n");
+    /*need to sort and then join the titles still (e.g., someArray.join("\n")  */
+    titles.sort();
+    const titleString = titles.join("\n");
 
-//     let textArea: HTMLInputElement | null = document.getElementById("displayArea") as HTMLInputElement | null;
-//     if (textArea) { textArea.innerHTML = titleString; }
-// }
+    let textArea: HTMLInputElement | null = document.getElementById("displayArea") as HTMLInputElement | null;
+    if (textArea) { textArea.innerHTML = titleString; }
+}
 
-// /**
-//  * 
-//  * @return {object} array holding all titles as elements
-//  */
-// export function findTitles(): string[] {
-
-
-//     //fix this to work according to the specs
-//     let titles = ["This is a test title", "Another test title", "And another test title for good measure"];  
-//     return titles;
-// }
+/**
+ * 
+ * @return {object} array holding all titles as elements.findTitles, which will find all the book titles in libraryBooks and return them in an
+alphabetically ordered array
+ */
 
 
-// /**
-//  * @returns {undefined} no return
-//  * Event handler for Add book button.  Creates and adds book to the library
-//  */
-// export function addBook():  void {
+export function findTitles(): string[] {
 
-//     console.log("finish the implementation -- get the author, create a book object, and add to the library array!!");
-// }
 
-// /**
-//  * 
-//  * @returns {string[]} find all  authors in libraryBooks and return them in alphabetically ordered array.
-//  */
-// export function findAuthors(): string[] {
-//     //implement this
-
-// }
-
-// /**
-//  * 
-//  * @returns {number[]} find all the libraryIDs in libraryBooks and return them in an alphabetically ordered array.
-// */
-// export function findIDs(): number[] {
-
+    const title: string[] = [];
+    for(let book of library){
+        title.push(book.title);
+    }
     
-//     //implement this
+    return title.sort();
+}
 
-// }
+/**
+ * @returns {undefined} no return
+ * Event handler for Add book button.  Creates and adds book to the library
+ */
+export function addBook(): void {
 
+    console.log("finish the implementation -- get the author, create a book object, and add to the library array!!");
+}
 
-// /**
-//  * @returns {book} new book object
-//  * createBook, which will take title, author, and libraryID as inputs.  It will create a new book object and add it to the library, 
-// which will be represented as a global array named libraryBooks.  createBook should return the newly created book.
-//  */
-// export function createBook(title: string, author: string, libraryID: number): Book {
-
-//     let libraryBooks = [title, author, libraryID];
-//     let Book = {libraryBooks};
-//     return Book;
-
-// }
-//     // // //Create the title for the html document
-//     // let bookTitle = document.createElement('label');
-//     // let bookTitleInput = document.createElement('input');
-//     // bookTitle.innerHTML =  "Book Title" ;
-//     // document.body.prepend(bookTitle);
-//     // bookTitleInput.value = title;
-//     // bookTitle.after(bookTitleInput);
+/**
+ * 
+ * @returns {string[]} find all  authors in libraryBooks and return them in alphabetically ordered array.
+ */
 
 
+export function findAuthors(): string[] {
+    // Loop throught the library books to access the authors
+    const authors: string[] = [];
+    for (let book of library) {
+        // Push the author of the book to the library
+        authors.push(book.author);
+    }
+    return authors;
+}
+
+/**
+ * 
+ * @returns {number[]} find all the libraryIDs in libraryBooks and return them in an alphabetically ordered array.
+*/
+
+export function findIDs(): number[] {
+
+    let ids: number[] = [];
+    //Loop through the library books to access the ids
+    for(let book of library){
+        // Push all the library ids to the idsa array
+        ids.push(book.libraryID);
+    }
+   const bookIds = ids.sort();
+   return bookIds;
+}
 
 
-//     // // // Create the first input element
-//     // let bookAuthor = document.createElement('label');
-//     // bookAuthor.innerHTML = "Author";
-//     // document.body.append(bookAuthor);
+/**
+ * @returns {book} new book object
+ * createBook, which will take title, author, and libraryID as inputs.  It will create a new book object and add it to the library, 
+which will be represented as a global array named libraryBooks.  createBook should return the newly created book.
+ */
 
-//     // // Create input for the book
-//     // let bookInput = document.createElement('input');
-//     // bookInput.value = author;
-//     // bookAuthor.after(bookInput);
+// Create a global array
+const libraryBooks: Book[] = [];
+export function createBook(title: string, author: string, libraryID: number): Book {
 
-//     // // Create an libraryId for the input
-
-//     // let libraryCard = document.createElement('label');
-//     // libraryCard.innerHTML = "libraryID";
-//     // document.body.append(libraryCard);
-
-//     // // Create an input for the library card
-//     // let libraryInput = document.createElement('input');
-//     // libraryInput.value = libraryID;
-
-//     // let createBook = [];
-//     // createBook.push(title);
-//     // createBook.push(author);
-//     // createBook.push(libraryCard);
-
-//     // const books = {createBook};
-//     // return books;
+    const newBook: Book = {
+        title: title,
+        author: author,
+        libraryID: libraryID,
+    }
+    //Add the books to the global array
+    libraryBooks.push(newBook);
+    return newBook;
+}
 
 
+/**
+ * 
+ * @returns {undefined} 
+ */
+export function scramble(): void {
+    console.log("implement scramble if you have time ...");
 
-
-// /**
-//  * 
-//  * @returns {undefined} 
-//  */
-// export function scramble(): void {
-//     console.log("implement scramble if you have time ...");
-
-// }
-
-
-
+}
