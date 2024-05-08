b9a224707eb21ef48aac70d6d1b2a3ba35891f64
sahrfearmacarthy <macarthysahralves>
2024-05-07 20:52:16 -0500

    Updated Resume

--- Portfolio/hero.js
+++ Portfolio/hero.js
@@ -274,3 +274,29 @@
 
 // // Start
 // setup();
+
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+import "dotenv/config";
+
+const GRID_API = process.env.sendGridAPI;
+
+const sgMail = require("@sendgrid/mail");
+if (GRID_API) {
+  sgMail.setApiKey(GRID_API);
+  const msg = {
+    to: "test@example.com", // Change to your recipient
+    from: "test@example.com", // Change to your verified sender
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
